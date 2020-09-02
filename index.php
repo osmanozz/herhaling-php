36513a9b970f180d5ed955ef99705f73836c094b
osmanozz <2120601@talnet.nl>
2020-09-02 16:45:00 +0200

    huiswerk week1

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+
+
+//$getal = 10;
+//echo gettype($getal);
+
+
+//$klas = ["Piet van Dijk", "Rembrandt van Rijn", "Vincent van Gogh", "Floris van Dyck"];
+//echo $klas[0];
+//echo '<br />';
+//var_dump($klas);
+
+
+// $klas = ["Piet van Dijk", "Rembrandt van Rijn", "Vincent van Gogh", "Floris van Dyck"];
+// for ($i=0; $i<count($klas); $i++) {
+//   echo $klas[$i].PHP_EOL;
+// }
+
+
+// $klas = ["Piet van Dijk", "Rembrandt van Rijn", "Vincent van Gogh", "Floris van Dyck"];
+// foreach ($klas as $i) {
+//   echo $i.PHP_EOL;
+// }
+
+
+// $bucket_list = array();
+// $hoeveelheid_activiteiten = readline('hoeveel activiteiten wilt u toevoegen aan de bucket_list? ');
+//
+// for ($i=0; $i < $hoeveelheid_activiteiten; $i++) {
+//   $activity = readline('Welke activiteiten wil je toevoegen ?');
+//   array_push($bucket_list, $activity);
+// }
+// foreach ($bucket_list as $value) {
+//   echo $value.PHP_EOL;
+// }
+// var_dump($bucket_list);
+//
+
+
+
+ ?>
